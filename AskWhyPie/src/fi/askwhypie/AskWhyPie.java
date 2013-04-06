c4f7b1abfdd577da395e2236408ec21a0e7f37fc
Morrow3 <joni.hauhia@cs.helsinki.fi>
2013-04-06 17:33:05 +0300

    MENU TOIMII!!!

--- AskWhyPie/src/fi/askwhypie/AskWhyPie.java
+++ AskWhyPie/src/fi/askwhypie/AskWhyPie.java
@@ -16,16 +16,12 @@ import org.newdawn.slick.tiled.TiledMap;
 */
 public class AskWhyPie extends BasicGame
 {
-     private Image grassMap;
-     private Animation sprite, up, down, left, right;
-     private float x = 34f, y = 34f;
- 
-     /** The collision map indicating which tiles block movement - generated based on tile properties */
-     private static final int SIZE = 34;
+    Menu m;
  
      public AskWhyPie()
      {
          super("AskWhy game");
+
      }
  
      public static void main(String[] args)
@@ -45,65 +41,18 @@ public class AskWhyPie extends BasicGame
      @Override
      public void init(GameContainer container) throws SlickException
      {
-         Image [] movementUp = {new Image("data/dude.png"), new Image("data/dude.png")};
-         Image [] movementDown = {new Image("data/dude.png"), new Image("data/dude.png")};
-         Image [] movementLeft = {new Image("data/dude.png"), new Image("data/dude.png")};
-         Image [] movementRight = {new Image("data/dude.png"), new Image("data/dude.png")};
-         int [] duration = {300, 300};         
-         grassMap = new Image("data/map.jpg");
- 
-          /*
-          * false variable means do not auto update the animation.
-          * By setting it to false animation will update only when
-          * the user presses a key.
-          */
-         up = new Animation(movementUp, duration, false);
-         down = new Animation(movementDown, duration, false);
-         left = new Animation(movementLeft, duration, false);
-         right = new Animation(movementRight, duration, false);
- 
-         // Original orientation of the sprite. It will look right.
-         sprite = right;
- 
-         // build a collision map based on tile properties in the TileD map
+         m = new Menu(container);
          
      }
  
      @Override
      public void update(GameContainer container, int delta) throws SlickException
      {
-         Input input = container.getInput();
-         if (input.isKeyDown(Input.KEY_UP))
-         {
-             sprite = up;
-             y-=1;
-             
-         }
-         else if (input.isKeyDown(Input.KEY_DOWN))
-         {
-             sprite = down;
-             y+=1;
-             
-         }
-         else if (input.isKeyDown(Input.KEY_LEFT))
-         {
-             sprite = left;
-             x-=1;
-             
-         }
-         else if (input.isKeyDown(Input.KEY_RIGHT))
-         {
-             sprite = right;
-             x+=1;
-             
-         }
+         
      }
- 
      public void render(GameContainer container, Graphics g) throws SlickException
      {
-         Menu m;
-         m = new Menu(container, g);
-         m.drawWholeMenu();
+         m.drawWholeMenu(g);
          //grassMap.draw();
          //sprite.draw((int)x, (int)y);
      }
