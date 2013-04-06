3527e9218aba6eec36fdd762f0ac9e6d2d004ddb
kristian.pulkkinen@cs.helsinki.fi <kurisu@iki.fi>
2013-04-06 20:28:45 +0300

    Entity-luokkaa uusittu rankasti

--- AskWhyPie/src/fi/askwhypie/Entity.java
+++ AskWhyPie/src/fi/askwhypie/Entity.java
@@ -1,6 +1,12 @@
 package fi.askwhypie;
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
 public class Entity {
     public static final int UP = 0;
@@ -8,19 +14,43 @@ public class Entity {
     public static final int LEFT = 2;
     public static final int RIGHT = 3;
     
-    private float x;
-    private float y;
+    private float x, y, width, height;
     private float speed;
     private int facing;
+    private int state;
+
     
-    private Animation[] animation= new Animation[4];
+    private ArrayList<Animation[]> animations;
     
-    
-    public Entity(float x, float y) {
+    public Entity(float x, float y, float width, float height) {
 	this.x = x;
 	this.y = y;
+	this.width = width;
+	this.height = height;
 	this.speed = 0;
 	this.facing = 0;
+	this.state = 0;
+	
+	animations = new ArrayList<Animation[]>();
+    }
+    
+    public void setAnimation(Animation anim, int facing, int state) {
+	while (animations.size() <= state)
+	    animations.add(new Animation[4]);
+	
+	animations.get(state)[facing] = anim;
+    }
+    
+    public Animation getAnimation() {
+	return animations.get(this.state)[this.facing];
+    }
+    
+    public Animation getAnimation(int facing) {
+	return animations.get(this.state)[facing];
+    }
+    
+    public Animation getAnimation(int facing, int state) {
+	return animations.get(state)[facing];
     }
 
     public float getX() {
@@ -76,6 +106,22 @@ public class Entity {
 	}
     }
     
+    public float getHeight() {
+	return height;
+    }
+
+    public void setHeight(float height) {
+	this.height = height;
+    }
+
+    public float getWidth() {
+	return width;
+    }
+
+    public void setWidth(float width) {
+	this.width = width;
+    }
+    
     public float getSpeed() {
 	return speed;
     }
@@ -92,11 +138,52 @@ public class Entity {
 	this.facing = facing;
     }
     
-    public Animation getAnimation(int facing) {
-	return animation[facing];
+    public int getState() {
+	return state;
     }
     
-    public void setAnimation(Animation anim, int facing) {
-	this.animation[facing] = anim;
+    public void setState(int state) {
+	this.state = state;
+    }
+    
+    
+    
+    
+    
+    
+    public static Image[] createImageArray(String directory, float rotation) {
+	File file = new File(directory);
+	
+	String[] fileNames = file.list();
+	Image[] images = new Image[fileNames.length];
+	
+	for (int i=0; i<fileNames.length; ++i) {
+	    try {
+		images[i] = new Image(directory+fileNames[i]);
+		images[i].rotate(rotation);
+	    } catch (SlickException ex) {
+		Logger.getLogger(AskWhyPie.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
+	
+	return images;
+    }
+    
+    public static Image[] createImageArray(SpriteSheet sheet, int[][] frames, float rotation) {
+	/* Esimerkki miten voi käyttää tätä metodia
+	Image[] playerUp    = createImageArray(sheet, new int[][]{{0, 3}, {1, 3}, {2, 3}, {1, 3}}, 0);
+	Image[] playerDown  = createImageArray(sheet, new int[][]{{0, 0}, {1, 0}, {2, 0}, {1, 0}}, 0);
+	Image[] playerLeft  = createImageArray(sheet, new int[][]{{0, 1}, {1, 1}, {2, 1}, {1, 1}}, 0);
+	Image[] playerRight = createImageArray(sheet, new int[][]{{0, 2}, {1, 2}, {2, 2}, {1, 2}}, 0);
+	*/
+	
+	Image[] images = new Image[frames[0].length];
+	
+	for (int i=0; i<images.length; ++i) {
+	    images[i] = sheet.getSprite(frames[i][0], frames[i][1]);
+	    images[i].setRotation(rotation);
+	}
+	
+	return images;
     }
 }
--- AskWhyPie/src/fi/askwhypie/Player.java
+++ AskWhyPie/src/fi/askwhypie/Player.java
@@ -1,11 +1,46 @@
 package fi.askwhypie;
 
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
 public class Player extends Entity {
     
+    public static final int STATE_WALK = 0;
+    public static final int STATE_DIE = 1;
     
     
     public Player(float x, float y) {
-	super(x, y);
+	super(x, y, 32, 64);
+	initPlayer();
     }
     
+    private void initPlayer() {
+	SpriteSheet sheet = null;
+	try {
+	    sheet = new SpriteSheet("data/player/skeleton.png", 32, 64);
+	} catch (SlickException ex) {
+	    Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+	}
+
+	Image[] playerUp    = new Image[]{sheet.getSprite(0, 3), sheet.getSprite(1, 3), sheet.getSprite(2, 3), sheet.getSprite(1, 3)};
+	Image[] playerDown  = new Image[]{sheet.getSprite(0, 0), sheet.getSprite(1, 0), sheet.getSprite(2, 0), sheet.getSprite(1, 0)};
+	Image[] playerLeft  = new Image[]{sheet.getSprite(0, 1), sheet.getSprite(1, 1), sheet.getSprite(2, 1), sheet.getSprite(1, 1)};
+	Image[] playerRight = new Image[]{sheet.getSprite(0, 2), sheet.getSprite(1, 2), sheet.getSprite(2, 2), sheet.getSprite(1, 2)};
+
+	setAnimation(new Animation(playerUp, 250), Player.UP, STATE_WALK);
+	setAnimation(new Animation(playerDown, 250), Player.DOWN, STATE_WALK);
+	setAnimation(new Animation(playerLeft, 250), Player.LEFT, STATE_WALK);
+	setAnimation(new Animation(playerRight, 250), Player.RIGHT, STATE_WALK);
+	
+	// Kuolinanimaatio tällä hetkellä kaikissa suunnissa 'ylöspäin kävely'
+	setAnimation(new Animation(playerUp, 250), Player.UP, STATE_DIE);
+	setAnimation(new Animation(playerUp, 250), Player.DOWN, STATE_DIE);
+	setAnimation(new Animation(playerUp, 250), Player.LEFT, STATE_DIE);
+	setAnimation(new Animation(playerUp, 250), Player.RIGHT, STATE_DIE);
+    }
 }
