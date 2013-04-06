2a6515d9331119ebf0ea086f9a8471edaabc934f
Hallo <ville.knuuttila@gmail.com>
2013-04-06 15:25:27 +0300

    the main functionality

--- AskWhyPie/src/fi/askwhypie/AskWhyPie.java
+++ AskWhyPie/src/fi/askwhypie/AskWhyPie.java
@@ -1,38 +1,108 @@
 package fi.askwhypie;
-import org.newdawn.slick.*;
 
-
+import org.newdawn.slick.Animation; 
+import org.newdawn.slick.AppGameContainer; 
+import org.newdawn.slick.BasicGame; 
+import org.newdawn.slick.GameContainer; 
+import org.newdawn.slick.Graphics; 
+import org.newdawn.slick.Image; 
+import org.newdawn.slick.Input; 
+import org.newdawn.slick.SlickException; 
+import org.newdawn.slick.tiled.TiledMap;  
+ 
+/**  
+*  
+* @author panos  
+*/
 public class AskWhyPie extends BasicGame
 {
+     private Image grassMap;
+     private Animation sprite, up, down, left, right;
+     private float x = 34f, y = 34f;
  
-  public AskWhyPie()
-  {
-     super("Hello World");
-  }
+     /** The collision map indicating which tiles block movement - generated based on tile properties */
+     private static final int SIZE = 34;
  
-  @Override
-  public void init(GameContainer gc) throws SlickException
-  {
+     public AskWhyPie()
+     {
+         super("AskWhy game");
+     }
  
-  }
+     public static void main(String [] arguments)
+     {
+         try
+         {
+             AppGameContainer app = new AppGameContainer(new AskWhyPie());
+             app.setDisplayMode(1312, 768, false);
+             app.start();
+         }
+         catch (SlickException e)
+         {
+             e.printStackTrace();
+         }
+     }
  
-  @Override
-  public void update(GameContainer gc, int delta) throws SlickException
-  {
+     @Override
+     public void init(GameContainer container) throws SlickException
+     {
+         Image [] movementUp = {new Image("data/dude.png"), new Image("data/dude.png")};
+         Image [] movementDown = {new Image("data/dude.png"), new Image("data/dude.png")};
+         Image [] movementLeft = {new Image("data/dude.png"), new Image("data/dude.png")};
+         Image [] movementRight = {new Image("data/dude.png"), new Image("data/dude.png")};
+         int [] duration = {300, 300};         
+         grassMap = new Image("data/map.jpg");
  
-  }
+          /*
+          * false variable means do not auto update the animation.
+          * By setting it to false animation will update only when
+          * the user presses a key.
+          */
+         up = new Animation(movementUp, duration, false);
+         down = new Animation(movementDown, duration, false);
+         left = new Animation(movementLeft, duration, false);
+         right = new Animation(movementRight, duration, false);
  
-  @Override
-  public void render(GameContainer gc, Graphics g) throws SlickException
-  {
-     g.drawString("Hello World", 100, 100);
-  }
+         // Original orientation of the sprite. It will look right.
+         sprite = right;
  
-  public static void main(String[] args) throws SlickException
-  {
-     AppGameContainer app = new AppGameContainer(new AskWhyPie());
+         // build a collision map based on tile properties in the TileD map
+         
+     }
  
-     app.setDisplayMode(800, 600, false);
-     app.start();
-  }
-}
+     @Override
+     public void update(GameContainer container, int delta) throws SlickException
+     {
+         Input input = container.getInput();
+         if (input.isKeyDown(Input.KEY_UP))
+         {
+             sprite = up;
+             y-=1;
+             
+         }
+         else if (input.isKeyDown(Input.KEY_DOWN))
+         {
+             sprite = down;
+             y+=1;
+             
+         }
+         else if (input.isKeyDown(Input.KEY_LEFT))
+         {
+             sprite = left;
+             x-=1;
+             
+         }
+         else if (input.isKeyDown(Input.KEY_RIGHT))
+         {
+             sprite = right;
+             x+=1;
+             
+         }
+     }
+ 
+     public void render(GameContainer container, Graphics g) throws SlickException
+     {
+         grassMap.draw();
+         sprite.draw((int)x, (int)y);
+     }
+ 
+ }
